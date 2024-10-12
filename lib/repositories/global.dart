572c7b42368b55399c9fc60eabe2185449814b77
John Brandon <johnbrandonlambino1232gmail.com>
2024-10-12 21:21:25 +0800

    dadad

--- lib/repositories/global.dart
+++ lib/repositories/global.dart
@@ -1,3 +1,3 @@
-const String baseUrl = 'http://192.168.100.216:8000/api';
-const String profileUrl = 'http://192.168.100.216:8000/';
+const String baseUrl = 'http://192.168.100.212:8000/api';
+const String profileUrl = 'http://192.168.100.212:8000/';
 
