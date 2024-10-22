e156070206df2418e259e2a7e4e1621a1d7c1957
John Brandon Lambino <johnbrandonlambino123@gmail.com>
2024-10-22 16:48:26 +0800

    merge

--- lib/repositories/global.dart
+++ lib/repositories/global.dart
@@ -1,2 +1,2 @@
-const String baseUrl = 'http://192.168.100.212:8000/api';
-const String profileUrl = 'http://192.168.100.212:8000/';
+const String baseUrl = 'http://192.168.100.222:8000/api';
+const String profileUrl = 'http://192.168.100.222:8000/';
